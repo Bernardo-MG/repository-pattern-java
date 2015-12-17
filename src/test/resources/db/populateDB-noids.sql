7aee45af3f9d5a713e1e0c151296aa408976a8ce
Bernardo <bmg>
2015-12-17 10:42:33 +0100

    Changed initialization scripts.

---
+++ src/test/resources/db/populateDB-noids.sql
@@ -0,0 +1,4 @@
+INSERT INTO test_entities (name) VALUES ('entity_1');
+INSERT INTO test_entities (name) VALUES ('entity_2');
+INSERT INTO test_entities (name) VALUES ('entity_3');
+INSERT INTO test_entities (name) VALUES ('entity_4');
