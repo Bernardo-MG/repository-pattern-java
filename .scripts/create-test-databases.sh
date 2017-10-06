86c5b715fbcd21c9d4052c1ae74d0566a3a9c64a
Bernardo <programming@wandrell.com>
2017-10-07 01:16:39 +0200

    Changed test DB names

--- .scripts/create-test-databases.sh
+++ .scripts/create-test-databases.sh
@@ -9,19 +9,11 @@ set -o nounset
 set -e
 
 # MySQL tables
-mysql -e "create database IF NOT EXISTS test_jpa_hibernate;" -uroot
-mysql -e "create database IF NOT EXISTS test_jpa_hibernate_modify;" -uroot
-mysql -e "create database IF NOT EXISTS test_spring_jdbc;" -uroot
-mysql -e "create database IF NOT EXISTS test_spring_jdbc_modify;" -uroot
-mysql -e "create database IF NOT EXISTS test_jpa_eclipselink;" -uroot
-mysql -e "create database IF NOT EXISTS test_jpa_eclipselink_modify;" -uroot
+mysql -e "create database IF NOT EXISTS jpa_test;" -uroot
+mysql -e "create database IF NOT EXISTS spring_jdbc_test;" -uroot
 
 # PostgreSQL tables
-psql -c 'create database test_jpa_hibernate;' -U postgres
-psql -c 'create database test_jpa_hibernate_modify;' -U postgres
-psql -c 'create database test_spring_jdbc;' -U postgres
-psql -c 'create database test_spring_jdbc_modify;' -U postgres
-psql -c 'create database test_jpa_eclipselink;' -U postgres
-psql -c 'create database test_jpa_eclipselink_modify;' -U postgres
+psql -c 'create database jpa_test;' -U postgres
+psql -c 'create database spring_jdbc_test;' -U postgres
 
 exit 0
