49e980ac994e073fd3f8fa149c3d185c74f7af9f
Bernardo <programming@wandrell.com>
2017-10-07 01:21:15 +0200

    Corrected test DB names

--- .scripts/create-test-databases.sh
+++ .scripts/create-test-databases.sh
@@ -9,11 +9,9 @@ set -o nounset
 set -e
 
 # MySQL tables
-mysql -e "create database IF NOT EXISTS jpa_test;" -uroot
-mysql -e "create database IF NOT EXISTS spring_jdbc_test;" -uroot
+mysql -e "create database IF NOT EXISTS repository_test;" -uroot
 
 # PostgreSQL tables
-psql -c 'create database jpa_test;' -U postgres
-psql -c 'create database spring_jdbc_test;' -U postgres
+psql -c 'create database repository_test;' -U postgres
 
 exit 0
